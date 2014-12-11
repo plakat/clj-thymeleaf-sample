7caf80f13ce3042485aa1addd76e582e86318fc3
plakat <github@plak.at>
2014-12-11 18:31:09 +0100

    comments added

--- src/clj_thymeleaf_sample/routes/home.clj
+++ src/clj_thymeleaf_sample/routes/home.clj
@@ -8,8 +8,10 @@
     {
       :status 200
       :headers {"Content-Type" "text/html;charset=utf-8"}
+      ;; request the view template by name without extension:
       :viewname "helloworld"
-      ;; the value of key :data is provided as a data map to the template:
+      ;; the value of key :data is provided as a data map to the template. Here all request parameters are passed on
+      ;; together with the backends result in the key :result (which can be named as you whish):
       :data (assoc params
               :result resultmap)
       }
